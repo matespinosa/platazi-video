703ea759e4e0a7aa906cf625459490151f87727c
matespinosa <matespinosa09@gmail.com>
2020-12-15 12:18:20 -0500

    añadiendo react-router

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API)
 
   return initialState.length === 0 ? <h1>loading...</h1> : (
@@ -51,4 +51,4 @@ const App = () => {
   )
 };
 
-export default App;
+export default Home;
